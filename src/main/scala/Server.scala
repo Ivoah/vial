24e6e94d079abeaf454e152f42a7e9fcdcc63f9a
Noah Rosamilia <ivoahivoah@gmail.com>
2022-07-12 11:16:19 -0400

    Fix imports and add listening message

--- src/main/scala/Server.scala
+++ src/main/scala/Server.scala
@@ -7,5 +7,8 @@ case class Server(router: Router, host: String = "127.0.0.1", port: Int = 8000) 
   private val server = HttpServer.create(new InetSocketAddress(host, port), 0)
   server.createContext("/", router.handler)
 
-  def serve(): Unit = server.start()
+  def serve(): Unit = {
+    server.start()
+    println(s"Listening on $host:$port")
+  }
 }
