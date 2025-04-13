84d41e2b02f651713ad60e2bacba07158bd76708
Noah Rosamilia <noah@ivoah.net>
2025-04-12 22:13:03 -0400

    URLDecode form values

--- vial/src/main/scala/Request.scala
+++ vial/src/main/scala/Request.scala
@@ -1,10 +1,11 @@
 package net.ivoah.vial
 
 import java.util.Base64
+import java.net.URLDecoder
 
 case class Request(headers: Map[String, Seq[String]], params: Map[String, String], body: Array[Byte]) {
   lazy val form: Map[String, String] = new String(body).split("&").collect {
-    case s"$key=$value" => key -> value
+    case s"$key=$value" => key -> URLDecoder.decode(value, "UTF-8")
   }.toMap
 
   lazy val auth: Option[(String, String)] = headers.get("Authorization").map {
